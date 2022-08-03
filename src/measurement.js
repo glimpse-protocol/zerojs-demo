ad833b1fab9394896d758cef4b13375390a8ff4e
Evan Hynes <evan.hynes@glimpseprotocol.io>
2022-08-03 14:27:16 +0100

    feat: add page load time measurement

--- src/measurement.js
+++ src/measurement.js
@@ -1,1 +1,9 @@
-console.log("I am measurement.js")
+function measurePageLoad() {
+  const pageLoad = document.getElementById("page-load-time")
+
+  const duration = performance.getEntriesByType("navigation")[0].duration
+  if (!duration) setTimeout(measurePageLoad, 0)
+  else pageLoad.innerText = `Page load time: ${Math.round(duration)}ms`
+}
+
+window.addEventListener("DOMContentLoaded", measurePageLoad)
