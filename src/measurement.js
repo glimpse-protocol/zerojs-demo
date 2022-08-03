e62ebc05b0a0da17fca13f1a1b54e531861947e9
Evan Hynes <evan.hynes@glimpseprotocol.io>
2022-08-03 15:32:02 +0100

    feat: add zero.js load time measurement

--- src/measurement.js
+++ src/measurement.js
@@ -1,9 +1,34 @@
-function measurePageLoad() {
+function measurePageLoad(count = 0) {
+  if (count >= 10) {
+    console.warn("Page load time was unable to be calculated")
+    return undefined
+  }
+
   const pageLoad = document.getElementById("page-load-time")
 
-  const duration = performance.getEntriesByType("navigation")[0].duration
-  if (!duration) setTimeout(measurePageLoad, 0)
-  else pageLoad.innerText = `Page load time: ${Math.round(duration)}ms`
+  const perf = performance.getEntriesByType("navigation")[0]
+
+  if (perf && perf.duration) {
+    pageLoad.innerText = `Page load time: ${Math.round(perf.duration)}ms`
+  } else setTimeout(() => measurePageLoad(count + 1), 0)
 }
 
-window.addEventListener("DOMContentLoaded", measurePageLoad)
+function mesasureZeroJsLoad(count = 0) {
+  if (count >= 10) {
+    console.warn("Zero.js load time was unable to be calculated")
+    return undefined
+  }
+
+  const zerojsLoad = document.getElementById("zerojs-load-time")
+
+  const perf = performance.getEntriesByName("https://cdn.glimpsezero.io/scripts/zero.js")[0]
+
+  if (perf && perf.duration) {
+    zerojsLoad.innerText = `Zero.js load time: ${Math.round(perf.duration)}ms`
+  } else setTimeout(() => mesasureZeroJsLoad(count + 1), 0)
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  measurePageLoad()
+  mesasureZeroJsLoad()
+})
