1a62e552051a69b0f33a4a6b72c1f5ff1c435b7d
Evan Hynes <evan.hynes@glimpseprotocol.io>
2022-08-04 17:01:06 +0100

    feat: make suggested changes before live demo

--- src/measurement.js
+++ src/measurement.js
@@ -13,33 +13,22 @@ function measurePageLoad(count = 0) {
   } else setTimeout(() => measurePageLoad(count + 1), 0)
 }
 
-function mesasureZeroJsLoad(count = 0) {
-  if (count >= 100) {
-    console.warn("Zero.js load time was unable to be calculated")
-    return undefined
-  }
-
-  const zerojsLoad = document.getElementById("zerojs-load-time")
-
-  const perf = performance.getEntriesByName("https://cdn.glimpsezero.io/scripts/zero.js")[0]
-
-  if (perf && perf.duration) {
-    zerojsLoad.innerText = Math.round(perf.duration)
-  } else setTimeout(() => mesasureZeroJsLoad(count + 1), 0)
-}
-
 window.addEventListener("DOMContentLoaded", () => {
   measurePageLoad()
-  mesasureZeroJsLoad()
 })
 
-window.addEventListener("message", ({ data }) => {
-  if (data === "zjs_serve") {
-    const adType = document.getElementById("ad-unit-path")
-    adType.innerText = "Sponsorship"
+window.addEventListener("message", (event) => {
+  console.log(event)
 
-    const adCarbon = document.getElementById("ad-unit-carbon-cost")
-    adCarbon.innerText = "0.1"
+  if (event.data === "zjs_serve") {
+    const pageLoadEmoji = document.getElementById("page-load-emoji")
+    pageLoadEmoji.innerHTML = "⚡"
+
+    const unitOne = document.getElementById("unit-01-type")
+    unitOne.innerText = "Prematch ✔️"
+
+    const unitTwo = document.getElementById("unit-02-type")
+    unitTwo.innerText = "Prematch ✔️"
 
     const carbonSaving = document.getElementById("total-carbon-savings")
     carbonSaving.innerText = "90"
