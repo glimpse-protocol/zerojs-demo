786ba9a7f4ead55322bb503d746337a5ad3ac7c9
Evan Hynes <evan.hynes@glimpseprotocol.io>
2022-08-04 15:56:16 +0100

    style: improve measurement setting via span tags

--- src/measurement.js
+++ src/measurement.js
@@ -9,7 +9,7 @@ function measurePageLoad(count = 0) {
   const perf = performance.getEntriesByType("navigation")[0]
 
   if (perf && perf.duration) {
-    pageLoad.innerText = `Page load time: ${Math.round(perf.duration)}ms`
+    pageLoad.innerText = Math.round(perf.duration)
   } else setTimeout(() => measurePageLoad(count + 1), 0)
 }
 
@@ -24,7 +24,7 @@ function mesasureZeroJsLoad(count = 0) {
   const perf = performance.getEntriesByName("https://cdn.glimpsezero.io/scripts/zero.js")[0]
 
   if (perf && perf.duration) {
-    zerojsLoad.innerText = `Zero.js load time: ${Math.round(perf.duration)}ms`
+    zerojsLoad.innerText = Math.round(perf.duration)
   } else setTimeout(() => mesasureZeroJsLoad(count + 1), 0)
 }
 
@@ -35,16 +35,13 @@ window.addEventListener("DOMContentLoaded", () => {
 
 window.addEventListener("message", ({ data }) => {
   if (data === "zjs_serve") {
-    const adType = document.getElementById("dynamic-ad-type")
-    adType.innerText = "Line Item: Sponsorship"
+    const adType = document.getElementById("ad-unit-path")
+    adType.innerText = "Sponsorship"
 
-    const adCarbon = document.getElementById("dynamic-ad-carbon")
-    adCarbon.innerText = "0.1g"
+    const adCarbon = document.getElementById("ad-unit-carbon-cost")
+    adCarbon.innerText = "0.1"
 
-    const carbonSaving = document.getElementById("dynamic-carbon-saving")
-    carbonSaving.innerText = "90% less carbon"
-
-    console.log("WHOOOO")
-    // update carbon / total carbon & line item type
+    const carbonSaving = document.getElementById("total-carbon-savings")
+    carbonSaving.innerText = "90"
   }
 })
