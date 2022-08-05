e6a4c2c618bb7156d2e990efe1eb392d060d055f
Evan Hynes <evan.hynes@glimpseprotocol.io>
2022-08-05 11:07:56 +0100

    feat: remove incorrect page load time measurement

--- src/measurement.js
+++ src/measurement.js
@@ -1,25 +1,4 @@
-function measurePageLoad(count = 0) {
-  if (count >= 100) {
-    console.warn("Page load time was unable to be calculated")
-    return undefined
-  }
-
-  const pageLoad = document.getElementById("page-load-time")
-
-  const perf = performance.getEntriesByType("navigation")[0]
-
-  if (perf && perf.duration) {
-    pageLoad.innerText = Math.round(perf.duration)
-  } else setTimeout(() => measurePageLoad(count + 1), 0)
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  measurePageLoad()
-})
-
 window.addEventListener("message", (event) => {
-  console.log(event)
-
   if (event.data === "zjs_serve") {
     const pageLoadEmoji = document.getElementById("page-load-emoji")
     pageLoadEmoji.innerHTML = "⚡"
